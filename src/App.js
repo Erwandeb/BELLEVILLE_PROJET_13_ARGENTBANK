a15d2f5a519e2c3b4b678a036622825e7875b79c
Erwan de Belleville <edebelleville@hotmail.fr>
2021-10-21 12:31:54 +0200

    building

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import NotFound from './pages/NotFound';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import './App.css';
+import HomePage from './pages/HomePage';
+import Login from './pages/Login';
+import Profil from './pages/Profil';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={HomePage}/>
+          <Route path="/login" exact component={Login}/>
+          <Route path="/profil" exact component={Profil}/>
+          <Route component={NotFound}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
