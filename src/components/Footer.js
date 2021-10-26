50e8b765cc5005a8f00c1472f962dddd983ed0d9
Erwan de Belleville <edebelleville@hotmail.fr>
2021-10-26 11:39:00 +0200

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className="footer-display">
             <footer className="footer">
-                <p className="footer-text">Copyright 2020 Argent Bank</p>
+                <p className="footer-text">Copyright-2020 Argent Bank</p>
             </footer>
         </div>
     );
