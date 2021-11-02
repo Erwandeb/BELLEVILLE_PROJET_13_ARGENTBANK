4bebb2b9bb7d716e5903f0b9321c15d88602862a
Erwan de Belleville <edebelleville@hotmail.fr>
2021-11-02 09:04:27 +0100

    modifs

--- src/App.js
+++ src/App.js
@@ -5,11 +5,50 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';
 import Profil from './pages/Profil';
+import { useEffect, useState } from 'react';
+import { UidContext } from './components/AppContext';
+import axios from 'axios';
 
 
 function App() {
+
+  const [uid, setUid] = useState(null);
+
+  useEffect(()=>{
+    const fetchToken = async () => {
+      await axios({
+        method: "post",
+        url: `${process.env.REACT_APP_API_URL}api/v1/user/profile`
+      })
+      .then((res)=> setUid(res.data))
+      .catch((res)=>console.log('no token'+ res))
+    }
+    fetchToken();
+  })
+
+/*
+let bearer = 'Bearer ' + JSON.parse(localStorage.getItem('token'));
+
+//payload is the data which you're trying to send to the api endpoint
+
+axios({
+    method: 'post',
+    url: `${process.env.REACT_APP_API_URL}api/v1/user/profile`,
+    headers: {
+      Authorization: bearer
+    },
+    data: payload
+  })
+    .then(response => response.json())
+    .then(json => json)
+    .catch(error => {
+      throw error;
+    });
+*/
+
   return (
     <div className="App">
+      <UidContext.Provider value={uid}>
       <BrowserRouter>
         <Switch>
           <Route path="/" exact component={HomePage}/>
@@ -18,6 +57,7 @@ function App() {
           <Route component={NotFound}/>
         </Switch>
       </BrowserRouter>
+      </UidContext.Provider>
     </div>
   );
 }
--- src/components/SignIncontent.js
+++ src/components/SignIncontent.js
@@ -17,8 +17,7 @@ const SignIncontent = () => {
             const usernameError = document.querySelector('.userName-error');
             const passwordError = document.querySelector('.password-error');
             */
-          
-
+        
             axios({
                 method:"post",
                 url:`${process.env.REACT_APP_API_URL}api/v1/user/login`,
@@ -42,44 +41,40 @@ const SignIncontent = () => {
 
 
     return (
-        
-            <main className="main bg-dark">
-                <section className="sign-in-content">
-                    <i className="fa fa-user-circle sign-in-icon"></i>
-                    <h1>Sign In</h1>
-                    <form action="" onSubmit={handleLogin}>
-                        <div className="input-wrapper">
-                            <label htmlFor="username">Username</label>
-                            <input 
-                                type="text" 
-                                id="username"
-                                onChange={(e)=>setUsername(e.target.value)}
-                                value={username}
-                            />
-                            <div className="userName-error"></div>
-                        </div>
-                        <div className="input-wrapper">
-                            <label htmlFor="password">Password</label >
-                            <input 
-                                type="password" 
-                                id="password"
-                                onChange={(e)=>setPassword(e.target.value)}
-                                value={password}
-                            />
-                            <div className="password-error"></div>
-                        </div>
-                        <div className="input-remember">
-                            <input 
-                                type="checkbox" 
-                                id="remember-me"
-                            />
-                            <label htmlFor="remember-me">Remember me</label>
-                        </div>
-                        <input type="submit" className="sign-in-button" value ="Sign in"/>
-                    </form>
-                </section>
-            </main>
-      
+        <section className="sign-in-content">
+            <i className="fa fa-user-circle sign-in-icon"></i>
+            <h1>Sign In</h1>
+            <form action="" onSubmit={handleLogin}>
+                <div className="input-wrapper">
+                    <label htmlFor="username">Username</label>
+                    <input 
+                        type="text" 
+                        id="username"
+                        onChange={(e)=>setUsername(e.target.value)}
+                        value={username}
+                    />
+                    <div className="userName-error"></div>
+                </div>
+                <div className="input-wrapper">
+                    <label htmlFor="password">Password</label >
+                        <input 
+                            type="password" 
+                            id="password"
+                            onChange={(e)=>setPassword(e.target.value)}
+                            value={password}
+                        />
+                    <div className="password-error"></div>
+                </div>
+                <div className="input-remember">
+                        <input 
+                            type="checkbox" 
+                            id="remember-me"
+                        />
+                        <label htmlFor="remember-me">Remember me</label>
+                </div>
+                <input type="submit" className="sign-in-button" value ="Sign in"/>
+            </form>
+        </section>
     );
 };
 
